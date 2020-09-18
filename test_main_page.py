6d11cfa68d38747ee83b05473b286c0d0130ae14
Artem Razinkin <massiveframe@gmail.com>
2020-09-19 00:15:54 +0300

    Added new asserts for login_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
-from .main_page import MainPage
-from .base_page import BasePage
-
+from pages.main_page import MainPage
+from pages.base_page import BasePage
+from pages.login_page import LoginPage
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    page = LoginPage(browser, link)
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
