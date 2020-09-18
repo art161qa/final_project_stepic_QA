fcb6e830492e84746ede6d9f8d96604a340bd829
Artem Razinkin <massiveframe@gmail.com>
2020-09-18 12:20:31 +0300

    Update assert and also added locators.py

---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+from .main_page import MainPage
+from .base_page import BasePage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+
